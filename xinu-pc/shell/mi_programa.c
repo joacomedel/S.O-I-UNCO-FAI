81f81a114605de0838a313da41aa9e3e55f29989
joacomedel <joaquinmedel1234@gmail.com>
2023-03-25 12:13:56 -0300

    Creaccion de procesos y eliminacion pasado un tiempo

--- xinu-pc/shell/mi_programa.c
+++ xinu-pc/shell/mi_programa.c
@@ -2,16 +2,24 @@
 void repetirA(void), repetirB(void);
 
 mi_programa() {
+int32 pidA = 0;
+int32 pidB = 0;
 printf("Inicio programa 1 repetidamente va a imprimir A\n");
-resume(create(repetirA,1024,20,"repetir A",0));
+pidA = create(repetirA,1024,20,"repetir A",0);
+resume(pidA);
 printf("Inicio programa 2 repetidamente va a imprimir B\n");
-resume(create(repetirB,1024,20,"repetir B",0));
+pidB = create(repetirB,1024,20,"repetir B",0);
+resume(pidB);
+sleep(10);
+kill(pidA);
+kill(pidB);
+printf("TERMINO");
 }
 void repetirA(void){
     while (1)
     {
         putc(CONSOLE, 'A');
-        sleep(20);
+        sleep(2);
     }
     printf("\n"); 
 }
@@ -19,7 +27,7 @@ void repetirB(void){
     while (1)
     {
         putc(CONSOLE, 'B');
-        sleep(20);
+        sleep(2);
     }
     printf("\n"); 
 }
